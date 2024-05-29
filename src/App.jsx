ff264dce8f170a1025c24cd78e1e9e3291d93a91
mirzamujezinovic <mikigithub@gmail.com>
2024-05-29 11:51:42 +0200

    Svasta po nesto(Navbar,Kartice i footer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css';
+import MyKartica from './components/Kartica/Kartica';
+// import MySentence from './components/MySentence/MySentence'
+// import Greeting from './components/Greeting/Greeting'
+import hotels from "./common/hotels.json"
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+    <header>
+    <div className="header">
+    <Navbar/>
+    </div>
+    </header>
+    <main>
+    <div className="hotels">
+    {
+    hotels.map((hotel)=>(
+      <MyKartica
+      key={hotel.id}
+      imageUrl={hotel.imageUrl}
+      ocena={hotel.ocena}
+      ime={hotel.ime}
+      lokacija={hotel.lokacija}
+      cena={hotel.cena}
+      staraCena={hotel.staraCena}
+      total={hotel.total}
+
+      />
+
+    )
+  )
+    }
+  </div>
+  
+    </main>
+    <footer>
+      <Footer/>
+    </footer>
+    </div>
     </>
   )
 }
