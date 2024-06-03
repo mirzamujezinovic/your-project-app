04070b1e8d74de06b72c0a910cba32aa4ada18c0
mirzamujezinovic <mikigithub@gmail.com>
2024-06-03 20:25:06 +0200

    Pravljenje pocetne straniceee

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import "./Home.css"
+function Home(){
+    return(
+        <>
+        <div className="home1">
+        <h1>Dobrodošli na Našu Aplikaciju</h1>
+                <p>
+                    Ovde možete pronaći najbolje hotele iz različitih delova sveta.
+                    Pogledajte našu ponudu i rezervišite svoj boravak danas!
+                    
+                    Naša platforma vam omogućava da istražite različite destinacije,
+                    pregledate ocene drugih korisnika i pronađete idealan smeštaj po vašim preferencijama.
+                
+                    Bez obzira da li tražite luksuzan odmor na plaži, poslovni smeštaj u urbanim centrima,
+                ili udoban boravak u ruralnim predelima, mi vam nudimo širok izbor hotela koji
+                odgovaraju svakom vašem ukusu i budžetu.
+                </p>
+                
+        </div>
+        </>
+    )
+}
+export default Home;
