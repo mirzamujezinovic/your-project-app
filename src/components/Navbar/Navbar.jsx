bb166bf5b49dbefc29a6f4cf1021c53a648e4aa3
mirzamujezinovic <mikigithub@gmail.com>
2024-06-03 20:21:28 +0200

    Nadogradnja headera

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
 import "./Navbar.css"
+import { NavLink } from "react-router-dom";
 import { FaServicestack } from "react-icons/fa";
 import { AiOutlineProject } from "react-icons/ai";
 import { GoCommentDiscussion } from "react-icons/go";
 import { MdOutlineLocalPhone } from "react-icons/md";
-import { FaSearch } from "react-icons/fa";
+import { BiSolidHotel } from "react-icons/bi";
+
 
 function Navbar(){
     return(
         <>
+        {/* https://pngimg.com/uploads/lamborghini/lamborghini_PNG10709.png */}
         <header>
         <div className="header">
+            <NavLink to="/" >
             <div className="logo">
-                <img src="https://pngimg.com/uploads/lamborghini/lamborghini_PNG10709.png" alt="slika"  />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Booking.com_logo.svg/2560px-Booking.com_logo.svg.png" alt="slika"  />
             </div>
-            <div className="info">
-                <div className="search"><FaSearch /><p>Search</p></div>
+            </NavLink>
+                <div className="info">
+                <NavLink to="/hotels" className={({isActive})=>(isActive ? "active-link" : "classic-link")}>
+                <div className="search"><BiSolidHotel /><p>Hotel</p></div>
+                </NavLink>
                 <div className="services" ><FaServicestack /><p>Services</p></div>
                 <div className="project" ><AiOutlineProject /><p>Projects</p></div>
                 <div className="aboutus" ><GoCommentDiscussion /><p>About Us</p></div>
