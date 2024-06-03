b7c969fe03ca19c5be19ab4cdbcd37d2455c77d7
mirzamujezinovic <mikigithub@gmail.com>
2024-06-03 20:25:30 +0200

    Update kartice za hotele

--- src/components/Kartica/Kartica.jsx
+++ src/components/Kartica/Kartica.jsx
@@ -1,3 +1,4 @@
+import { Route } from "react-router-dom";
 import "./Kartica.css"
 import { MdLocalHotel } from "react-icons/md";
 function MyKartica(props){
@@ -8,14 +9,15 @@ function MyKartica(props){
             <img src={props?.imageUrl}/>
         </div>
         <div className="hotel-info">
-            <div className="ocena"><strong>{props?.ocena}/10 </strong>Exceptional (317 reviews)</div>
-        
+        <div className="ocena"><strong>{props?.ocena}/10 </strong>Exceptional (317 reviews)</div>
         <div className="hotel-ime">{props?.ime}</div>
         <div className="lokacija">{props?.lokacija}</div>
         <div className="cena">${props?.cena} <span className="stara-cena">{props?.staraCena}$</span></div>
         <div className="total-cena">${props?.total} total includes taxes & fees</div>
-        <div className="member-cena"><MdLocalHotel />Show hotel
-        </div>
+        <button className="card-price" onClick={props.onClick}>
+        <MdLocalHotel />Show hotel
+        </button>
+        
     </div>
     </div> 
     </>
