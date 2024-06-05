54bcb9a2b44ca00c5709a4c5bcc76ff843485c57
mirzamujezinovic <mikigithub@gmail.com>
2024-06-05 20:02:32 +0200

    Home pocetna.Stavljena slika preko celog ekrana i stavljen tekst sa leve.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,20 +2,21 @@ import "./Home.css"
 function Home(){
     return(
         <>
-        <div className="home1">
-        <h1>Dobrodošli na Našu Aplikaciju</h1>
+        <div className="home-page">
+            <div className="home-tekst">
+                <h1>Dobrodošli na Našu Aplikaciju</h1>
                 <p>
                     Ovde možete pronaći najbolje hotele iz različitih delova sveta.
                     Pogledajte našu ponudu i rezervišite svoj boravak danas!
-                    
+                </p>   
+                <p>
                     Naša platforma vam omogućava da istražite različite destinacije,
                     pregledate ocene drugih korisnika i pronađete idealan smeštaj po vašim preferencijama.
-                
-                    Bez obzira da li tražite luksuzan odmor na plaži, poslovni smeštaj u urbanim centrima,
-                ili udoban boravak u ruralnim predelima, mi vam nudimo širok izbor hotela koji
-                odgovaraju svakom vašem ukusu i budžetu.
                 </p>
-                
+                </div>
+        </div>
+        <div className="home-under">
+            <h1>TEKST ISPOD SLIKEEE</h1>
         </div>
         </>
     )
