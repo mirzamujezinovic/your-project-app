35d4d71890ec4c6d325d92c1ab7888831ed6175b
mirzamujezinovic <mikigithub@gmail.com>
2024-06-05 20:02:55 +0200

    Popravljen hotels

---
+++ src/pages/Hotels/Hotels/Hotels.jsx
@@ -0,0 +1,31 @@
+import "./Hotels.css"
+import hotels from "../../../common/hotels.json"
+import MyKartica from "../../../components/Kartica/Kartica";
+
+
+
+function HotelDetails(){
+    return(
+        <>
+        <div className='hotels1'>
+            {hotels.map((hotel)=>(
+              <MyKartica
+              key={hotel.id}
+              imageUrl={hotel.imageUrl}
+              ocena={hotel.ocena}
+              ime={hotel.ime}
+              lokacija={hotel.lokacija}
+              cena={hotel.cena}
+              staraCena={hotel.staraCena}
+              total={hotel.total}
+              onClick={() => (window.location.href = `/hotels/${hotel.id}`)}
+              
+              />
+              
+            ))}
+          </div>
+    </>
+    )
+}
+
+export default HotelDetails;
