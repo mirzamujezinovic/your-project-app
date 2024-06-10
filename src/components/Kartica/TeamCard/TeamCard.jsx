4904a0fe7efa5da43ae7addc8bf14d45fe475540
mirzamujezinovic <mikigithub@gmail.com>
2024-06-10 20:06:18 +0200

    pravljenje team kartica

--- src/components/Kartica/TeamCard/TeamCard.jsx
+++ src/components/Kartica/TeamCard/TeamCard.jsx
@@ -1,37 +1,41 @@
-/* eslint-disable react/prop-types */
-import "./TeamCard.css"
-import { useEffect,useState } from "react"
+import "./TeamCard.css";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
-function TeamCard(props){
-    const [showMore, setShowMore] = useState(false)
-    const [teamDescription, setTeamDescription] = useState("")
-    
-    useEffect(()=>{
-        if(showMore){
-            setTeamDescription("Ovaj tim ne osnovan 1925. godine. Najtrofejniji je klub u engleskoj...")
-        } else{
-            setTeamDescription("")
-        }
-    }, [showMore]);
-    return(
-        <div className={showMore ? "teamCard showMore" : "teamCard"} style={{ height: showMore ? 170 : 80 }}>
-        <h2 className="h2">{props.name}</h2>
-        <p className="p">{props.points}</p>
-        <p className="p">{props.matches}</p>
-        <p className="p">{props.wins}</p>
-        <p className="p">{props.draws}</p>
-        <p className="p">{props.losses}</p>
-        <p className="p">
-          <button onClick={() => setShowMore(!showMore)} className="show">
-            {showMore ? "Sakrij" : "Pokaži"}
-          </button>
+function TeamCard(props) {
+  const [showMore, setShowMore] = useState(false);
+  const [teamDescription, setTeamDescription] = useState("");
+
+  useEffect(() => {
+    if (showMore) {
+      setTeamDescription("Ovaj tim je osnovan 1925. godine. Najtrofejniji je klub u Engleskoj...");
+    } else {
+      setTeamDescription("");
+    }
+  }, [showMore]);
+
+  return (
+    <div className={showMore ? "teamCard showMore" : "teamCard"} style={{ height: showMore ? 170 : 80 }}>
+      <h2 className="h2">{props.name}</h2>
+      <p className="p">{props.points}</p>
+      <p className="p">{props.matches}</p>
+      <p className="p">{props.wins}</p>
+      <p className="p">{props.draws}</p>
+      <p className="p">{props.losses}</p>
+      <p className="p">
+        <button onClick={() => setShowMore(!showMore)} className="show">
+          {showMore ? "Sakrij" : "Pokaži"}
+        </button>
         </p>
-        {showMore && <p className="p text">{teamDescription}</p>}
+        <p className="p">
+        <button className="trashIcon" onClick={props.onDelete}>
+          <FaRegTrashAlt />
+        </button>
       
-      </div>
-      
-    )
-    
+      </p>
+      {showMore && <p className="p text">{teamDescription}</p>}
+    </div>
+  );
 }
 
 export default TeamCard;
--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -1,25 +1,35 @@
-import teams from "../../common/teams.json"
-import TeamCard from "../../components/Kartica/TeamCard/TeamCard"
-import "./Teams.css"
+import teams from "../../common/teams.json";
+import TeamCard from "../../components/Kartica/TeamCard/TeamCard";
+import "./Teams.css";
+import { useState } from "react";
 
-function Teams(){
-    return(
-        <>
-        <div className="list">
-            {teams.map((value)=>{
-                return(
-                    <TeamCard
-                    name={value["team_name"]}
-                    points={`${value["points"]}pts`}
-                    wins={value.wins}
-                    loses={value.loses}
-                    draws={value.draws}
-                    losses={value.losses}
-                    matches={value.matches_played}/>
-                )
-            })}
-        </div>
-        </>
-    )
+function Teams() {
+  const [teamsList, setTeamsList] = useState(teams);
+
+  const handleDelete = (teamName) => {
+    const updatedTeams = teamsList.filter((team) => team.team_name !== teamName);
+    setTeamsList(updatedTeams);
+  };
+
+  return (
+    <>
+      <div className="list">
+        {teamsList.map((value) => (
+          <TeamCard
+            id={value.id}
+            name={value.team_name}
+            points={`${value.points}pts`}
+            wins={value.wins}
+            loses={value.loses}
+            draws={value.draws}
+            losses={value.losses}
+            matches={value.matches_played}
+            onDelete={() => handleDelete(value.team_name)}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
-export default Teams
+
+export default Teams;
