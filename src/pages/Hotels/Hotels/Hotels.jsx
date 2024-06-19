94794972d6b2a51ed21317357053c357a16336b5
mirzamujezinovic <mikigithub@gmail.com>
2024-06-19 19:51:23 +0200

    Nadogradnja hotela

--- src/pages/Hotels/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels/Hotels.jsx
@@ -1,10 +1,19 @@
 import "./Hotels.css"
 import hotels from "../../../common/hotels.json"
 import MyKartica from "../../../components/Kartica/HotelCard/Kartica";
+import Pagination from "../../../components/Pagination/Pagination";
+import { useState } from "react";
 
 
 
 function HotelDetails(){
+    const [page, setPage] = useState(1)
+    const numOfHotelsPerPage = 8
+
+    const numOfHotels = hotels.length
+    const numOfPages = Math.ceil(numOfHotels / 8)
+    console.log(numOfPages)
+
     return(
         <>
         <div className='hotels1'>
@@ -19,11 +28,16 @@ function HotelDetails(){
               staraCena={hotel.staraCena}
               total={hotel.total}
               onClick={() => (window.location.href = `/hotels/${hotel.id}`)}
-              
               />
               
-            ))}
+            )).slice(numOfHotelsPerPage * (page - 1), numOfHotelsPerPage * page)
+              //.slice(0,8) prikazivanje prvih 8 hotela 11111111
+              //.slice(8,16) prikazivanje drugih 8 hotela 22222222
+              //.slice(16,24) prikazivanje trecih 8 hotela 33333333
+            }
+
           </div>
+          <Pagination numOfPages={numOfPages} page={page} setPage={setPage} />
     </>
     )
 }
